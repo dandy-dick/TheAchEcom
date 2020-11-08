c416d590e1ac8f815fa2f9d0939ce73738676610
dandy-dick <73324453+dandy-dick@users.noreply.github.com>
2020-11-08 14:45:23 +0700

    Add files via upload
    File query để tạo dữ liệu test + trigger để ứng dụng hoạt động

---
+++ triggers.sql
@@ -0,0 +1,23 @@
+﻿create TRIGGER OnProductInsert ON Products
+FOR INSERT
+AS begin
+	DECLARE @id int;
+	DECLARE cursorProduct CURSOR FOR  -- khai báo con trỏ cursorProduct
+	SELECT Id FROM inserted     -- dữ liệu trỏ tới
+
+
+	OPEN cursorProduct                -- Mở con trỏ
+	FETCH NEXT FROM cursorProduct     -- Đọc dòng đầu tiên
+		INTO @id
+	
+	WHILE @@FETCH_STATUS = 0          --vòng lặp WHILE khi đọc Cursor thành công
+	BEGIN
+		insert into ProductActivityTrackings(ProductId, OrderedCount,ReviewCount,SoldCount,ViewCount,RatingAvg)
+		values (@id, 0,0,0,0,0)
+		FETCH NEXT FROM cursorProduct     -- Đọc dòng đầu tiên
+		INTO @id
+
+	END
+	CLOSE cursorProduct              -- Đóng Cursor
+	DEALLOCATE cursorProduct         -- Giải phóng tài nguyên
+end
